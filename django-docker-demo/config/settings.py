73545e4dee84fd4f3d4f58f069de3d705dea16ba
Riju <avisekdey1994@gmail.com>
2026-01-24 17:35:22 +0530

    Changed config to load dubug value form env

--- django-docker-demo/config/settings.py
+++ django-docker-demo/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+5d312&1f+xj31c8lv%)wcu+dmw==1d)z$-u*a)(lubk+dk7k*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "1"
 
 ALLOWED_HOSTS = ["*"]
 
