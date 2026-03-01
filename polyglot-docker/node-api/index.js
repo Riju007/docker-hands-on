57bf68e3dd0e6f18a5b868b0d8cc859d7b043c25
AVISEK DEY <avisekdey1994@gmail.com>
2026-03-01 18:06:41 +0530

    Created log functionality for node

--- polyglot-docker/node-api/index.js
+++ polyglot-docker/node-api/index.js
@@ -1,14 +1,26 @@
 const express = require("express")
 const app = express()
 
+function logJson(level, message, extra = {}) {
+    const log = {
+        "service": "node",
+        level,
+        message,
+        timestamp: new Date().toISOString(),
+        ...extra
+    }
+    console.log(JSON.stringify(log));
+}
+
 app.get("/health", (req, res) => {
-    res.json({ service: "node", status: "ok" })
+    logJson("info", "health check called");
+    res.json({ service: "node", status: "ok" });
 });
 
 app.get("/info", (req, res) => {
-    res.json({ language: "Node.js", runtime: process.version })
+    res.json({ language: "Node.js", runtime: process.version });
 });
 
 app.listen(3000, () => {
-    console.log("Node api is running on port 3000")
+    console.log("Node api is running on port 3000");
 });
