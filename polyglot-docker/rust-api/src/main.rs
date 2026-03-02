c974efc6c77835d29523be3cdd0ad52a7a672b81
AVISEK DEY <avisekdey1994@gmail.com>
2026-03-03 01:04:26 +0530

    Added tracing for rust log

--- polyglot-docker/rust-api/src/main.rs
+++ polyglot-docker/rust-api/src/main.rs
@@ -1,14 +1,18 @@
 use axum::{Router, routing::get};
 use serde_json::json;
+use tracing::{info};
+use tracing_subscriber::FmtSubscriber;
 
 async fn health() -> String {
-    println!("{}", serde_json::json!({
-        "service": "rust",
-        "level": "info",
-        "message": "health check called"
-    }));
+    println!(
+        "{}",
+        serde_json::json!({
+            "service": "rust",
+            "level": "info",
+            "message": "health check called"
+        })
+    );
     json!({"service": "rust", "status": "ok"}).to_string()
-    
 }
 
 async fn info() -> String {
@@ -19,8 +23,10 @@ async fn info() -> String {
 #[tokio::main]
 async fn main() {
     // initialize tracing
-    tracing_subscriber::fmt::init();
-
+    // tracing_subscriber::fmt::init();
+    let subscriber = FmtSubscriber::builder().finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    info!(service = "rust", message = "Server started");
     let app = Router::new()
         .route("/health", get(health))
         .route("/info", get(info));
